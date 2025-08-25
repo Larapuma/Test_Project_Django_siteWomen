0bdd55685a7e75cce15d582f757aa423bfceff87
SwagoSlaw <n.plechkov@yandex.ru>
2025-08-25 23:21:07 +0300

    Создана первая модель

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Women(models.Model):
+    title = models.CharField(max_length=255)#поле типа Varchar в таблице women
+    content = models.TextField(blank=True)#Позволяет не заносить туда что-то с самого начала, чтобы можно было добавить и сразу и потом
+    time_create = models.DateTimeField(auto_now_add=True)#Автоматически заполняет это поле в момент добавления этой записи(заполняет датой занесения)
+    time_update = models.DateTimeField(auto_now=True)#Это поле меняется каждое новое обновление
+    is_published = models.BooleanField(default=True)
